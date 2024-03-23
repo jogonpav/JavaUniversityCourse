9efcf1d7f0ce1ad9ff32afcefedbbf5efe3ca002
jogonpav <josepabonvera@gmail.com>
2024-03-23 17:23:14 -0500

    Refactor name project

--- JavaUniversityCourse/src/NotesExerciseJava/NotesExerciseJava.java
+++ JavaUniversityCourse/src/NotesExerciseJava/NotesExerciseJava.java
@@ -2,13 +2,13 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
  */
-package javauniversitycourse;
+package NotesExerciseJava;
 
 /**
  *
  * @author josep
  */
-public class JavaUniversityCourse {
+public class NotesExerciseJava {
 
     /**
      * @param args the command line arguments
